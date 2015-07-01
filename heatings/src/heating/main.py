873ce3ef619f5bedf4a0b67f9f2348e4d8a061f6
kojambo <koj@posteo.de>
2015-07-01 22:18:24 +0200

    restructured results files in folder

--- heatings/src/heating/main.py
+++ heatings/src/heating/main.py
@@ -1,7 +1,7 @@
 import scrapy.cmdline
 
 def main():
-    scrapy.cmdline.execute(argv=['scrapy', 'crawl', 'heating','-o items.csv'])
+    scrapy.cmdline.execute(argv=['scrapy', 'crawl', 'heating','-o output/items.csv'])
 
 if  __name__ =='__main__':
     main()
